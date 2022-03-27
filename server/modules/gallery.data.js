231ad05585022fa91ae001fb30c929dd915a0365
Jason Inouye <jasonjinouye@gmail.com>
2022-03-27 00:48:19 -0500

    added photo description and styling

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/loralei1.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/loralei2.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/aandjwedding.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/aandjitaly.jpeg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/loralei1.jpg', description: 'Backyard fun in the sandbox', likes: 0 },
+    { id: 2, path: 'images/loralei2.jpg', description: 'Loralei\'s first taste of cotton candy. "THIS...IS COTTON CANDY?!?"', likes: 0 },
+    { id: 3, path: 'images/aandjwedding.jpeg', description: 'Fireworks during Jennie and Ross\'s wedding', likes: 0 },
+    { id: 4, path: 'images/aandjitaly.jpeg', description: 'Italian Winery during Ross and Sarah\'s wedding', likes: 0 }
 ];
 
 module.exports = galleryItems;
